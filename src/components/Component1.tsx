e1908e9c03bb79a55ff4f1dc21cf4f183b3f825d
TerryFunggg <terryyessfung@gmail.com>
2021-04-22 13:27:24 +0800

    feat: update component 1

--- src/components/Component1.tsx
+++ src/components/Component1.tsx
@@ -5,9 +5,21 @@ export default function Component1() {
   const { dialogIsActive, handleDialog, dialogContent } = useContext(
     DialogContext
   );
+
   return (
     <div className="flex justify-center items-center h-screen m-auto">
-      <button className="bg-blue-800 text-white py-3 px-6 text-2xl font-bold rounded-xl focus:outline-none hover:bg-blue-600 transition ease-out duration-500">
+      <button
+        className="bg-blue-800 text-white py-3 px-6 text-2xl font-bold rounded-xl focus:outline-none hover:bg-blue-600 transition ease-out duration-500"
+        onClick={() =>
+          handleDialog({
+            title: "Hi",
+            description: "This message is came from component 1",
+            onOK: () => {
+              console.log("YOOOOO");
+            },
+          })
+        }
+      >
         Click me
       </button>
     </div>
