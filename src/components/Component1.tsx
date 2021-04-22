8938b463f54a6235c92e6f9d930b7edcc6303326
TerryFunggg <terryyessfung@gmail.com>
2021-04-22 13:19:10 +0800

    create a component1 for test our dialog component

---
+++ src/components/Component1.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { DialogContext } from "./DialogContext";
+
+export default function Component1() {
+  const { dialogIsActive, handleDialog, dialogContent } = useContext(
+    DialogContext
+  );
+  return (
+    <div className="flex justify-center items-center h-screen m-auto">
+      <button className="bg-blue-800 text-white py-3 px-6 text-2xl font-bold rounded-xl focus:outline-none hover:bg-blue-600 transition ease-out duration-500">
+        Click me
+      </button>
+    </div>
+  );
+}
