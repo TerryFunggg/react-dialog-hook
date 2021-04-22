006728168ff5b9b2e8cfce89dbabe24b9b07cc43
TerryFunggg <terryyessfung@gmail.com>
2021-04-22 13:27:04 +0800

    fix the wrong parm position

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -55,8 +55,7 @@ export default function MyDoalog() {
               </Dialog.Title>
               <div className="mt-2">
                 <p className="text-sm text-gray-500">
-                  Your payment has been successfully submitted. We’ve sent your
-                  an email with all of the details of your order.
+                  {dialogContent?.description || "Are your sure?"}
                 </p>
               </div>
 
@@ -66,7 +65,7 @@ export default function MyDoalog() {
                   className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
                   onClick={() => dialogContent?.onOK() || {}}
                 >
-                  {dialogContent?.description || "Are your sure?"}
+                  Confirm
                 </button>
               </div>
             </div>
